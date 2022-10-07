09f04232fc2b923a1ccc0c6d359aaa00cf1522bf
Jonas Quintiens <r0831281@student.thomasmore.be>
2022-10-07 14:45:02 +0200

    oefenmoment 1

--- MusicStore/Data/StoreContext.cs
+++ MusicStore/Data/StoreContext.cs
@@ -13,12 +13,19 @@ namespace MusicStore.Data
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Album> Albums { get; set; }
 
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Genre>().ToTable("Genre");
             modelBuilder.Entity<Artist>().ToTable("Artist");
             modelBuilder.Entity<Album>().ToTable("Album");
 
+            modelBuilder.Entity<CartItem>().ToTable("CartItem");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItem");
         }
     }
 }
