dce1781a17c39017b90efa01ef25e49bb25f3cbc
r0831281 <r0831281@student.thomasmore.be>
2022-10-27 23:24:03 +0200

    m

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Data;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreContext _context;
+        public StoreController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> ListGenres()
+        {
+
+            var genres = await _context.Genres.OrderBy(g => g.Name).ToListAsync();
+            return View(genres);
+
+        }
+
+        public async Task<IActionResult> ListAlbums(int? id)
+        {
+            var storeContext = _context.Albums.Where(a => a.GenreID == id).Include(a => a.Artist).Include(a => a.Genre);
+            return View(await storeContext.ToListAsync());
+        }
+    }
+}
